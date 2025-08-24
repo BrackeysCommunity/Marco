d8a6456abfeb2811e2b6f01fc9be7e9b0bb11893
Oliver Booth <oliver@booth.dev>
2025-08-24 13:34:09 +0100

    feat: add /anonmacro command

--- Marco/Commands/MacroCommand.cs
+++ Marco/Commands/MacroCommand.cs
@@ -23,28 +23,39 @@ internal sealed class MacroCommand : ApplicationCommandModule
         _cooldownService = cooldownService;
     }
 
+    [SlashCommand("anonmacro", "Executes an macro anonymously, hiding the invocation.", false)]
+    [SlashRequireGuild]
+    public async Task AnonymousMacroAsync(InteractionContext context,
+        [Option("macro", "The name of the macro.", true)] [Autocomplete(typeof(MacroAutocompleteProvider))] string macroName)
+    {
+        Macro? macro = await VerifyMacroAsync(context, macroName);
+        if (macro is null)
+        {
+            return;
+        }
+
+        var builder = new DiscordMessageBuilder();
+        string response = macro.Response;
+
+        if (response.StartsWith("@silent "))
+        {
+            response = response[8..];
+            builder.SuppressNotifications();
+        }
+
+        builder.WithContent(response);
+        await context.CreateResponseAsync("The macro has been sent.", true).ConfigureAwait(false);
+        await context.Channel.SendMessageAsync(builder).ConfigureAwait(false);
+    }
+
     [SlashCommand("macro", "Executes a macro.")]
     [SlashRequireGuild]
     public async Task MacroAsync(InteractionContext context,
-        [Option("macro", "The name of the macro.", true)] [Autocomplete(typeof(MacroAutocompleteProvider))]
-        string macroName)
+        [Option("macro", "The name of the macro.", true)] [Autocomplete(typeof(MacroAutocompleteProvider))] string macroName)
     {
-        if (!_macroService.TryGetMacro(context.Guild, macroName, out Macro? macro))
+        Macro? macro = await VerifyMacroAsync(context, macroName);
+        if (macro is null)
         {
-            await context.CreateResponseAsync($"The macro `{macroName}` doesn't exist.", true).ConfigureAwait(false);
-            return;
-        }
-
-        DiscordChannel channel = context.Channel;
-
-        if (macro.ChannelId.HasValue && macro.ChannelId.Value != channel.Id)
-        {
-            await context.CreateResponseAsync($"The macro `{macroName}` cannot be executed here.", true).ConfigureAwait(false);
-            return;
-        }
-        if (_cooldownService.IsOnCooldown(channel, macro))
-        {
-            await context.CreateResponseAsync($"The macro `{macroName}` is on cooldown because it was very recently executed.", true).ConfigureAwait(false);
             return;
         }
 
@@ -59,6 +70,33 @@ internal sealed class MacroCommand : ApplicationCommandModule
 
         builder.WithContent(response);
         await context.CreateResponseAsync(builder).ConfigureAwait(false);
-        _cooldownService.UpdateCooldown(channel, macro);
+        _cooldownService.UpdateCooldown(context.Channel, macro);
+    }
+
+    private async Task<Macro?> VerifyMacroAsync(InteractionContext context, string macroName)
+    {
+        if (!_macroService.TryGetMacro(context.Guild, macroName, out Macro? macro))
+        {
+            await context.CreateResponseAsync($"The macro `{macroName}` doesn't exist.", true).ConfigureAwait(false);
+            return null;
+        }
+
+        DiscordChannel channel = context.Channel;
+
+        if (macro.ChannelId.HasValue && macro.ChannelId.Value != channel.Id)
+        {
+            await context.CreateResponseAsync($"The macro `{macroName}` cannot be executed here.", true).ConfigureAwait(false);
+            return null;
+        }
+
+        if (_cooldownService.IsOnCooldown(channel, macro))
+        {
+            await context
+                .CreateResponseAsync($"The macro `{macroName}` is on cooldown because it was very recently executed.", true)
+                .ConfigureAwait(false);
+            return null;
+        }
+
+        return macro;
     }
 }
